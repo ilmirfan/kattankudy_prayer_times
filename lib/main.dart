ae02f78b378416ff60eb0e570520f184797e1a8b
Irfan Ishak <ilmirfan@gmail.com>
2022-03-06 05:06:37 +0530

    firebase cloud messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:kkyprayertime/prayertime.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent, //Color(0xffFFF6EC), // status bar color
